596a736f354e2f1c890bbd6e559efcb92000b496
yonara ferreira <yonara.fd@gmail.com>
2023-05-16 13:37:03 -0300

    First commit

---
+++ src/test/java/br/com/projetoNarah/demo/ProjetoDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoNarah.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
